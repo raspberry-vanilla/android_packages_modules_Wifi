f0c6785b24b6c20ac4b2fa3a02238a6324a13f3e
Konsta <konsta09@gmail.com>
2021-12-14 16:10:13 +0200

    HACK: rpi4: disable ANQP requests
    * https://github.com/lineage-rpi/android_kernel_brcm_rpi/issues/6
    
    Change-Id: If056e5394ee170a0b68ba6bb4ce0b34b93295218

--- service/java/com/android/server/wifi/ClientModeImpl.java
+++ service/java/com/android/server/wifi/ClientModeImpl.java
@@ -1861,7 +1861,8 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
 
     @Override
     public boolean requestAnqp(String bssid, Set<Integer> anqpIds, Set<Integer> hs20Subtypes) {
-        return mWifiNative.requestAnqp(mInterfaceName, bssid, anqpIds, hs20Subtypes);
+        //return mWifiNative.requestAnqp(mInterfaceName, bssid, anqpIds, hs20Subtypes);
+        return false;
     }
 
     @Override
